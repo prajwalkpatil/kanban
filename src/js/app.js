22d81434df288d8ddbef6ba6a821f1bac4d8d8e6
Prajwal Patil <prajwalpatilk@gmail.com>
2023-07-26 11:18:36 +0530

    Add functions for new list and card

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+const board = document.querySelector('.board');
 const cards = document.querySelectorAll('.card');
 const lists = document.querySelectorAll('.list');
 let draggedCard = null;
@@ -10,6 +11,7 @@ const cardHandler = (card) => {
         draggedFromList = card.parentElement;
         if (!draggedCard) {
             draggedCard = card;
+            draggedCard.style.opacity = 0.6;
             console.log(`${card.innerHTML} is being dragged!`)
         }
     })
@@ -24,8 +26,8 @@ const listHandler = (list) => {
         greyedList = list.querySelector(".list-items");
         greyedList.style.backgroundColor = "#E9EAEC";
     })
-
 }
+
 for (let card of cards) {
     cardHandler(card);
 }
@@ -34,13 +36,36 @@ for (let list of lists) {
     listHandler(list);
 }
 
+const createNewList = (title = 'Untitled') => {
+    let newList = document.createElement('div');
+    newList.classList.add('list');
+    let listTitle = document.createElement('div');
+    listTitle.classList.add('list-title');
+    listTitle.textContent = title;
+    let listItems = document.createElement('div');
+    listItems.classList.add('list-items');
+    newList.appendChild(listTitle);
+    newList.appendChild(listItems);
+    listHandler(newList);
+    board.appendChild(newList);
+}
+
+const createNewCard = (text = 'Card') => {
+    let newCard = document.createElement('div');
+    newCard.textContent = text;
+    newCard.classList.add('card');
+    newCard.draggable = true;
+    cardHandler(newCard);
+    return newCard;
+}
+
 document.addEventListener("dragend", () => {
     if (draggedToList && draggedCard) {
         draggedToList.appendChild(draggedCard);
+        draggedCard.style.opacity = 1;
     }
     draggedCard = null;
     draggedFromList = null;
     draggedToList = null;
     greyedList.style.backgroundColor = "#FFFFFF";
-    console.log("Drag ended");
 })
